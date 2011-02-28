b1d94807e3d73549b75b00da2876eaba42eda254
till <till@php.net>
2011-02-28 15:29:01 +0100

    * la la

--- www/test.php
+++ www/test.php
@@ -2,22 +2,10 @@
 
 <p>Readable? <?php var_dump(is_readable('/etc')); ?></p>
 
-<ul>
-<?php
-foreach (glob('/etc/*') as $item) {
-	echo "<li>{$item}</li>";
-}
-?>
-</ul>
-
 <h2>/mnt/orchestra-virtualized/home/</h2>
-<ul>
-<?php
-foreach (glob('/mnt/orchestra-virtualized/home/*') as $item) {
-    echo "<li>{$item}</li>";
-}
-?>
-</ul>
+<pre>
+<?php echo nl2br(shell_exec('ls -lah /mnt/orchestra-virtualized/home')); ?>
+</pre>
 
 <h2>ls -lah /etc/init.d</h2>
 <pre>
