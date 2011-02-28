5210f9d0a7b929fcc0ea505e6dd341735c30f4bb
till <till@php.net>
2011-02-28 15:34:39 +0100

    * html5 spy

---
+++ www/html5.php
@@ -0,0 +1,11 @@
+<?php
+echo nl2br(shell_exec('ls -lah /mnt/orchestra-virtualized/home/html5valentine'));
+?>
+<hr />
+
+<?php
+echo nl2br(shell_exec('ls -lah /mnt/orchestra-virtualized/home/html5valentine/public'));
+?>
+
+
+
